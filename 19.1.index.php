825e43e1b2d4fd0d61d2ce8f70ca824d4dce2550
kapil <kapil0123@gmail.com>
2021-07-02 17:58:59 +0530

    adding lamp stack yml

---
+++ 19.1.index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ansible Test Page</title>
+</head>
+
+<body>
+    <h6>The page will show current date</h6>
+    <?php
+
+    echo "The current date is: ";
+    echo(date("Y-m-d",time()));
+?>
+
+</body>
+
+</html>
